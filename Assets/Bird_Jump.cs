a4e4bd856d02cc57679d36d2f509e742b39e2ef6
Xe-Kinesis <lordkinesis@naver.com>
2021-12-11 23:24:47 +0900

    Bird_Jump Script

---
+++ Assets/Bird_Jump.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird_Jump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start 입니다.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update 입니다.");
+        
+    }
+}
