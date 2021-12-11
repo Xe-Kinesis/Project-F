aec2cbd562974090f16aadf3bf3529a87bcaae78
Xe-Kinesis <lordkinesis@naver.com>
2021-12-11 23:38:23 +0900

    Buird Jump SC

--- Assets/Bird_Jump.cs
+++ Assets/Bird_Jump.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Bird_Jump : MonoBehaviour
 {
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start 입니다.");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Update 입니다.");
+        if(Input.GetMouseButtonDown(0)){
+            rb.velocity = Vector2.up * 3;  // (0,3)      Vector2 = 2D세상 
+        }
         
     }
 }
